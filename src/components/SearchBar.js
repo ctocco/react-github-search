fb0ed282b5c1ddf7153467f0ae0571ad263eb3c3
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-01-20 18:39:28 +0100

    adding changes to bootstrap and adding components

---
+++ src/components/SearchBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class SearchBar extend React.Component {
+
+    state {term: ''} //#endregion
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+
+        )
+    };
+};
+
+
+export default SearchBar;
