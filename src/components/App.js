2842fdd0a3b3624e5d47c8fe61e4a28be5108057
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-01-21 21:25:40 +0100

    fixed delete array issue

--- src/components/App.js
+++ src/components/App.js
@@ -7,25 +7,21 @@ class App extends React.Component {
     
     state = {profiles: []};
 
-    // onSearchSubmit = async (term) => {
-        
-    // const response = await github.get('/users/', {
-    //     params: {query: term}
-    // },'client_id=c521f38a3fe316933134&client_secret=969af97602977ba1ceafe7bad98d76911cc58dc1' );
-    // this.setState({ profiles: response })
-    // }
-
     onSearchSubmit = async (term) => {
         const response = await github.get(`/users/${term}?client_id=c521f38a3fe316933134&client_secret=969af97602977ba1ceafe7bad98d76911cc58dc1` );
         this.setState({ profiles: [...this.state.profiles, response.data]});
-        console.log(this.state.profiles)
         }
     
-    onButtonClick = (id) => {
-        
-        console.log(id)
+    onButtonClick = (profile) => {
+        const removeFav = [...this.state.profiles].filter((el) => 
+        el.id !== profile.id)
+        this.setState({
+          profiles: removeFav,
+        })
     }
- 
+    
+    
+
     render() {
     return (
         <div className="container" style={{ marginTop: '50px'}}>
--- src/components/ProfileList.js
+++ src/components/ProfileList.js
@@ -3,39 +3,38 @@ import React from 'react';
 const ProfileList = props => {
 
     // const image = props.profiles["avatar_url"]
-
+    
 
     console.log(props.profiles)
     const image = props.profiles.map( (profile) => {
-     return <div className="card mb-3 mt-3">
+       
+     return <div key={profile.id} className="card mb-3 mt-3">
                 <h1 className="card-header">Username: {profile.login}</h1>
-                <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <h6 class="card-subtitle text-muted">Support card subtitle</h6>
+                <div className="card-body">
+                <h5 className="card-title">Special title treatment</h5>
+                <h6 className="card-subtitle text-muted">Support card subtitle</h6>
             </div>
                 <div className="container">
                 <div className="row">
                     <div className="col-sm-4"><img id={profile.id} style={{height: "100%", width: "100%"}} src={profile["avatar_url"]} alt="user" /></div>
                 
                 <div className="col-sm-4">
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item remove"></li>
-                            <li class="list-group-item remove"></li>
-                            <li class="list-group-item remove"></li>
-                            <li class="list-group-item remove"></li>
-                            <li class="list-group-item remove"></li>
-                            <li class="list-group-item remove"></li>
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item remove"></li>
+                            <li className="list-group-item remove"></li>
+                            <li className="list-group-item remove"></li>
+                            <li className="list-group-item remove"></li>
+                            <li className="list-group-item remove"></li>
+                            <li className="list-group-item remove"></li>
                         </ul>
                 </div>
                 <div className="col-sm-4 pr-5">
-                <a className="btn btn-info btn-lg" href="#" role="button" onClick={()=> props.onButtonClick(profile.id)}>Remove User</a>
+                <a className="btn btn-info btn-lg" href="#" role="button" onClick={()=> props.onButtonClick(profile)}>Remove User</a>
                 </div>
-
             </div>
- 
         </div>
-        <div class="card-footer text-muted block mt-3">Profile created on 
-       {' '}{profile.created_at}
+            <div className="card-footer text-muted block mt-3">Profile created on 
+            {' '}{profile.created_at}
   </div>
     </div>
     });
