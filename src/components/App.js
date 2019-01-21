595df0b6925ccdadf48adff2c45afa09d9f61cc1
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-01-21 15:45:34 +0100

    adding onclick to remove cards

--- src/components/App.js
+++ src/components/App.js
@@ -17,21 +17,21 @@ class App extends React.Component {
 
     onSearchSubmit = async (term) => {
         const response = await github.get(`/users/${term}?client_id=c521f38a3fe316933134&client_secret=969af97602977ba1ceafe7bad98d76911cc58dc1` );
-        const array = []
-        array.push(response)
-        console.log(array)
-        this.setState({ profiles: array });
-        console.log(response)
-        
+        this.setState({ profiles: [...this.state.profiles, response.data]});
+        console.log(this.state.profiles)
         }
     
-
+    onButtonClick = (id) => {
+        
+        console.log(id)
+    }
+ 
     render() {
     return (
         <div className="container" style={{ marginTop: '50px'}}>
         <Header />
         <SearchBar onSubmit={this.onSearchSubmit} />
-        <ProfileList profiles={this.state.profiles} />
+        <ProfileList onButtonClick={this.onButtonClick} profiles={this.state.profiles} />
         </div>
         );
     }
--- src/components/ProfileList.js
+++ src/components/ProfileList.js
@@ -2,13 +2,45 @@ import React from 'react';
 
 const ProfileList = props => {
 
-    console.log(props.profiles[0].profiles)
-   
-//  const profiles = 
-//  });
-    // console.log(profiles)
-return <div>
-    </div>;
+    // const image = props.profiles["avatar_url"]
+
+
+    console.log(props.profiles)
+    const image = props.profiles.map( (profile) => {
+     return <div className="card mb-3 mt-3">
+                <h1 className="card-header">Username: {profile.login}</h1>
+                <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <h6 class="card-subtitle text-muted">Support card subtitle</h6>
+            </div>
+                <div className="container">
+                <div className="row">
+                    <div className="col-sm-4"><img id={profile.id} style={{height: "100%", width: "100%"}} src={profile["avatar_url"]} alt="user" /></div>
+                
+                <div className="col-sm-4">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item remove"></li>
+                            <li class="list-group-item remove"></li>
+                            <li class="list-group-item remove"></li>
+                            <li class="list-group-item remove"></li>
+                            <li class="list-group-item remove"></li>
+                            <li class="list-group-item remove"></li>
+                        </ul>
+                </div>
+                <div className="col-sm-4 pr-5">
+                <a className="btn btn-info btn-lg" href="#" role="button" onClick={()=> props.onButtonClick(profile.id)}>Remove User</a>
+                </div>
+
+            </div>
+ 
+        </div>
+        <div class="card-footer text-muted block mt-3">Profile created on 
+       {' '}{profile.created_at}
+  </div>
+    </div>
+    });
+    
+return <div>{image}</div>;
 
 }
 
