dc9a35eff523cee743fb7b260e7aeda8fca86897
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-01-23 10:31:58 +0100

    adding notes for improvement

--- src/components/App.js
+++ src/components/App.js
@@ -4,28 +4,35 @@ import Header from './Header'
 import SearchBar from './SearchBar';
 import ProfileList from './ProfileList'
 class App extends React.Component {
-    
-    state = {profiles: []};
+
+    state = { profiles: [],
+    // turnModalOn: null,
+    // modalMessage null, 
+
+};
 
     onSearchSubmit = async (term) => {
-        const response = await github.get(`/users/${term}?client_id=c521f38a3fe316933134&client_secret=969af97602977ba1ceafe7bad98d76911cc58dc1` );
-        this.setState({ profiles: [...this.state.profiles, response.data]});
-        }
-    
+        const response = await github.get(`/users/${term}?client_id=c521f38a3fe316933134&client_secret=969af97602977ba1ceafe7bad98d76911cc58dc1`);
+        this.setState({ profiles: [...this.state.profiles, response.data] });
+        // put your if statements here 
+        // set boolean to true or false 
+
+    }
+
     onButtonClick = (id) => {
         const removeFav = [...this.state.profiles].filter((el) => el.id !== Number(id))
         this.setState({
-          profiles: removeFav,
+            profiles: removeFav,
         })
     }
-    
+
     render() {
-    return (
-        <div className="container" style={{ marginTop: '50px'}}>
-        <Header />
-        <SearchBar onSubmit={this.onSearchSubmit} />
-        <ProfileList onButtonClick={this.onButtonClick} profiles={this.state.profiles} />
-        </div>
+        return (
+            <div className="container" style={{ marginTop: '50px' }}>
+                <Header />
+                <SearchBar onSubmit={this.onSearchSubmit} />
+                <ProfileList onButtonClick={this.onButtonClick} profiles={this.state.profiles} />
+            </div>
         );
     }
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,25 +1,27 @@
 import React from 'react';
 
+ 
+export default ;
 class SearchBar extends React.Component {
 
-    state = {term: ''}; //#endregion
-    
+    state = { term: '' }; //#endregion
+
 
     onFormSubmit = event => {
         event.preventDefault();
         this.props.onSubmit(this.state.term)
-        this.setState({term: ''})
+        this.setState({ term: '' })
     };
 
     render() {
-        return ( <div className="container">
-           
+        return (<div className="container">
+
             <form onSubmit={this.onFormSubmit} className="form-group" >
                 <div className="row">
-                 
+
                     <label className="col-form-label col-form-label-lg" htmlFor="inputLarge">Type in a username below to get github user details</label>
-                    <input className="form-control form-control-lg" type="text" placeholder="Enter your search here" id="inputLarge" value={this.state.term}  onChange={(event) => this.setState({term: event.target.value})}/>
-                    
+                    <input className="form-control form-control-lg" type="text" placeholder="Enter your search here" id="inputLarge" value={this.state.term} onChange={(event) => this.setState({ term: event.target.value })} />
+
                     <button type="submit" className="btn btn-primary btn-block" >Submit</button>
 
                 </div>
