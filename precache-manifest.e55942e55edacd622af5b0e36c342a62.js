bb161851e04f3db42a5810d066325f30a39bb3ed
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-03-01 09:28:59 +0100

    Updates

---
+++ precache-manifest.e55942e55edacd622af5b0e36c342a62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea1ad28b40d48a892339",
+    "url": "/react-github-search/static/css/main.b0a317af.chunk.css"
+  },
+  {
+    "revision": "ea1ad28b40d48a892339",
+    "url": "/react-github-search/static/js/main.ea1ad28b.chunk.js"
+  },
+  {
+    "revision": "9b284ed9d2b5c20e1f0e",
+    "url": "/react-github-search/static/js/1.9b284ed9.chunk.js"
+  },
+  {
+    "revision": "dc2fcbcf50b6a205baf7",
+    "url": "/react-github-search/static/js/runtime~main.dc2fcbcf.js"
+  },
+  {
+    "revision": "7b720e8c03e65a7ffd97c8faba3c34f0",
+    "url": "/react-github-search/index.html"
+  }
+];
